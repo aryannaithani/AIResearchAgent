c39b52dbf87b66c63151afe040c810f40487760d
aryannaithani <aryannaithani1085@gmail.com>
2025-09-27 03:11:05 +0530

    Single Endpoint

--- static/script.js
+++ static/script.js
@@ -91,7 +91,14 @@ const messagesContainer = document.getElementById('messages');
 
     let content = message.text;
     if (content.startsWith('http')) {
-        content = `<a href="${content}" target="_blank">Download Report</a>`;
+        content = `<a href="${content}" target="_blank" class="download-button">
+            <svg class="download-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+                <polyline points="7,10 12,15 17,10"/>
+                <line x1="12" y1="15" x2="12" y2="3"/>
+            </svg>
+            Download Report
+        </a>`;
     }
 
     messageDiv.innerHTML = `
